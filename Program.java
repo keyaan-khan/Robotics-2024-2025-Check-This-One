cb2a80b96d8db357e2628c11a7d61920eb662395
keyaan kho <kkhan46@outlook.com>
2024-08-07 19:58:14 -0400

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,17 +1,6 @@
 public class Program {
     public static void main(String[] args) {
-        double x = 111; 
-
-        if (x < 100) {
-            x = x + 50;
-        }
-
-    
-        if (x > 100) {
-            x = x / 20;
-        }
-
-       
-        System.out.println("Final value of x: " + x);
+        // 9 inches
+        /* Because i hate being tall */
     }
 }
