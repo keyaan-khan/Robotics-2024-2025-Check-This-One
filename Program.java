1a48b5ec46bec22f958a5eeec22656256275bb10
keyaan kho <kkhan46@outlook.com>
2024-08-07 19:09:11 -0400

    Update Program.java

--- Program.java
+++ Program.java
@@ -2,22 +2,14 @@ public class Program {
     public static void main(String[] args) {
         
         final String immutableVariable = "Immutable String";
-  
-        
-        int[] mutableVariable = {1, 2, 3};
- 
 
-       
+        int[] mutableVariable = {1, 2, 3};
+
         System.out.println("Immutable variable: " + immutableVariable);
-        
 
         System.out.print("Mutable variable: ");
-        
-        for (int i : mutableVariable) {
-            System.out.print(i + " ");
-           
-        }
-        System.out.println();
-        
+
+        System.out.println(java.util.Arrays.toString(mutableVariable));
     }
 }
+
