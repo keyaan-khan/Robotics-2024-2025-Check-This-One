cc1d45125b684cf7614358e3be2a09898feb1045
keyaan kho <kkhan46@outlook.com>
2024-08-07 19:51:37 -0400

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,28 +1,17 @@
 public class Program {
     public static void main(String[] args) {
-        double x = 80.5;
+        double x = 111; 
 
         if (x < 100) {
-            x = x + 50.5;
+            x = x + 50;
         }
 
-        if (x >= 100) {
-            if (x <= 150) {
-                x = x / 20.0;
-            } else {
-                x = x % 20.0;
-            }
+    
+        if (x > 100) {
+            x = x / 20;
         }
 
-        if (x == 0) {
-            x = x + 100.0;
-        } else if (x < 10) {
-            x = x * 10.0;
-        } else {
-            x = x - 5.0;
-        }
-
-        System.out.println("final value of x: " + x);
+       
+        System.out.println("Final value of x: " + x);
     }
 }
-
