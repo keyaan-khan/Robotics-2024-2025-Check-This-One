c5b7bc6aba22baa1716367016bec88b38da7fa65
keyaan kho <kkhan46@outlook.com>
2024-08-08 19:18:06 -0400

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,6 +1,38 @@
 public class Program {
+
+   
+    public static class Person {
+        
+        int age = 16;
+
+       
+        public void printAge() {
+            System.out.println(age);
+        }
+
+        
+        public void makeOlder() {
+            age += 1;
+        }
+
+        
+        public void makeYounger() {
+            age -= 1;
+        }
+    }
+
+    
     public static void main(String[] args) {
-        // 9 inches
-        /* Because i hate being tall */
+       
+        Person person = new Person();
+
+       
+        person.printAge();
+
+        
+        person.makeOlder();
+
+       
+        person.printAge();
     }
 }
