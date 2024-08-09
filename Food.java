d793fef5cb65ae270c13f5508aef392ac7980be8
keyaan kho <kkhan46@outlook.com>
2024-08-09 19:10:12 -0400

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,10 @@
+public class Food {
+
+    public int expectedMininoven() = 40;
+
+    public static void main(String[] args) {
+       Food lasagna = new Food();
+        
+        System.out.println("The lasagna should be in the oven for " + lasagna.expectedMininoven() + " minutes.");
+    }
+}
